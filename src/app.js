fc346360c09ffb3553ee2352aa7c8e3cd5a16055
Davi dos Santos Freitas <davidossantosfreitas19@gmail.com>
2026-03-06 15:30:04 -0300

    style: run prettier to style code

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 app.use(express.json());
-    
-app.get("/", (request, response) => {
-  response.status(200).json({message: "Hello world"})
-})
+
+app.get('/', (request, response) => {
+  response.status(200).json({ message: 'Hello world' });
+});
 
 export default app;
