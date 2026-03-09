afb11a84b27c183e7110770e68fd20613a49551d
Davi dos Santos Freitas <davidossantosfreitas19@gmail.com>
2026-03-09 14:46:03 -0300

    build: configure cookies

---
+++ src/utils/cookies.js
@@ -0,0 +1,17 @@
+export const cookies = {
+  getOptions: () => ({
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'Strict',
+    maxAge: 30 * 60 * 1000, // 30 minutes
+  }),
+  set: (res, name, value, options = {}) => {
+    res.cookie(name, value, { ...cookies.getOptions(), ...options });
+  },
+  clear: (res, name, options = {}) => {
+    res.clearCookie(name, { ...cookies.getOptions(), ...options });
+  },
+  get: (req, name) => {
+    return req.cookies[name];
+  },
+};
